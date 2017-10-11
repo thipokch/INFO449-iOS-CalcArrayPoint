a32868b0b4f35fb02a5777a54c3325baf51dae26
Thipok Cholsaipant <thipok.ch@gmail.com>
2017-10-10 17:49:38 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Thipok Cholsaipant on 10/10/17.
+//  Copyright © 2017 Thipok Cholsaipant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
